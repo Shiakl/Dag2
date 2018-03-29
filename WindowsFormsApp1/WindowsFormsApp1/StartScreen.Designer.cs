f9fb09f230df88dbeae1fe95d85150227f7f41c9
ShiAkl <shivam_akloe@hotmail.com>
2018-03-29 12:49:53 +0200

    StartScreen added with functionality and references

---
+++ WindowsFormsApp1/WindowsFormsApp1/StartScreen.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Vang_de_volger
+{
+    partial class StartScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartGame = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(208, 87);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(232, 62);
+            this.StartGame.TabIndex = 0;
+            this.StartGame.Text = "Start the game";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(208, 195);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(232, 62);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StartScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.StartGame);
+            this.Name = "StartScreen";
+            this.Text = "StartScreen";
+            this.Load += new System.EventHandler(this.StartScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Button Exit;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/StartScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vang_de_volger
+{
+    public partial class StartScreen : Form
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void StartScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
